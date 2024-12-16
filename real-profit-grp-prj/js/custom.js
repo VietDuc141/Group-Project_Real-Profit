83aac5c8e8ddbe23dc5f5450c4106c00d9c090e5
VietDuc <ducnvm.22bi13095@usth.edu.vn>
2024-12-17 02:04:38 +0700

    update learning page

--- real-profit-grp-prj/js/custom.js
+++ real-profit-grp-prj/js/custom.js
@@ -30,17 +30,3 @@
     
   })(window.jQuery);
 
-  // OPEN LESSONS
-  function toggleLesson(element) {
-    const lessonContent = element.nextElementSibling; // Lấy phần nội dung của bài học
-    const toggleSymbol = element.querySelector(".toggle-symbol"); // Lấy dấu "+" hoặc "-"
-  
-    if (lessonContent.style.display === "block") {
-      lessonContent.style.display = "none"; // Ẩn nội dung
-      toggleSymbol.textContent = "+"; // Đổi về dấu "+"
-    } else {
-      lessonContent.style.display = "block"; // Hiện nội dung
-      toggleSymbol.textContent = "-"; // Đổi sang dấu "-"
-    }
-  }
-  
